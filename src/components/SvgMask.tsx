d30b1be045deb6b405b3d169e5d94fa35b545d80
Wajih Jmaiel <swaaj@users.noreply.github.com>
2023-08-25 13:35:14 +0200

    Update SvgMask.tsx

--- src/components/SvgMask.tsx
+++ src/components/SvgMask.tsx
@@ -37,9 +37,8 @@ interface State {
   animation: Animated.Value
   canvasSize: ValueXY
   previousPath: string
+  d: string
 }
-
-const IS_WEB = Platform.OS !== 'web'
 
 export class SvgMask extends Component<Props, State> {
   static defaultProps = {
@@ -125,13 +124,9 @@ export class SvgMask extends Component<Props, State> {
     const d = this.getPath()
     this.rafID = requestAnimationFrame(() => {
       if (this.mask && this.mask.current) {
-        if (IS_WEB) {
-          // @ts-ignore
-          this.mask.current.setNativeProps({ d })
-        } else {
-          // @ts-ignore
-          this.mask.current._touchableNode.setAttribute('d', d)
-        }
+        this.setState({
+          d,
+        })
       }
     })
   }
@@ -205,7 +200,7 @@ export class SvgMask extends Component<Props, State> {
             fill={this.props.backdropColor}
             strokeWidth={0}
             fillRule='evenodd'
-            d={this.firstPath}
+            d={this.state.d}
             opacity={this.state.opacity as any}
           />
         </Svg>
